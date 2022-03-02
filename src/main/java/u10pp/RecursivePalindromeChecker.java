79d6e6d0af26588adfc54100cf66ece2b91a6d03
Eric Hsieh <eric.kf.hsieh@gmail.com>
2022-03-02 11:19:42 -0800

    remove solution

--- src/main/java/u10pp/RecursivePalindromeChecker.java
+++ src/main/java/u10pp/RecursivePalindromeChecker.java
@@ -1,15 +1,2 @@
 package u10pp;
-public class RecursivePalindromeChecker {
-    /**
-     * 
-     * @param s
-     * @return
-     */
-    public static boolean isPalindrome(String s) {
-        if(s.length() <= 1) {
-            return true;
-        }
-        boolean firstLastMatch = s.charAt(0) == s.charAt(s.length()-1);
-        return firstLastMatch && isPalindrome(s.substring(1, s.length()-1));
-    }
-}
+
